046bee4bfef951692b5c4083bce63fc11b278523
yamabato <harutakutsukawa0601@gmail.com>
2023-08-30 13:06:10 +0000

    fix: move contact.js to /scripts

---
+++ scripts/contact.js
@@ -0,0 +1,6 @@
+function sendContact(){
+    content = document.getElementById("contact-textarea").value;
+    contactType = document.getElementById("contact-type-select").value;
+
+    console.log(content, contactType);
+}
